bb11ecd5a05f327481d325fec5de6c6347826b46
ujjwalhonmai <37949030+ujjwalhonmai@users.noreply.github.com>
2019-08-15 20:41:15 +0530

    second

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(window).on("load",function(){
     $(".loader .inner").fadeOut(1000,function(){
         $(".loader").fadeOut(1250);
     });
+    $(".items").isotope({
+        filter:'*',
+        animationOptions:{
+            duration:1500,
+            easing:'linear',
+            queue:false
+        }
+    })
 })
 $(document).ready(function(){
     $('#slides').superslides({
@@ -56,14 +64,7 @@ $(document).ready(function(){
         }
     })
     $("[data-fancybox]").fancybox()
-    $(".items").isotope({
-        filter:'*',
-        animationOptions:{
-            duration:1500,
-            easing:'linear',
-            queue:false
-        }
-    })
+  
     $(".filters a").click(function(){
         $(".filters .current").removeClass('current')
         $(this).addClass('current')
